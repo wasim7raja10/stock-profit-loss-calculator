4f2f78491a861af136817b2e852c20ac4ad01eb5
Wasim Raja <wasim7raja10@gmail.com>
2022-09-15 22:03:17 +0530

    added new validation

--- src/helper/helperfunctions.js
+++ src/helper/helperfunctions.js
@@ -1,6 +1,6 @@
 export function validateInput(...inputs) {
   for (const input of inputs) {
-    if (input === "") return false;
+    if (input === ""  || Number(input) < 0) return false;
   }
   return true;
 }
